b147b143625ef0fed8848a32e2871a8df76c2a20
Albert Zeyer <albzey@gmail.com>
2022-08-20 23:25:25 +0200

    cleanup

--- tui_editor/editor.py
+++ tui_editor/editor.py
@@ -1,37 +1,9 @@
-#!/usr/bin/env python3
-
 """
 Simple TUI editor
 
-Python TUI?
-
-What I want:
-- simple multi-line editor
-- not whole screen but only partial
-- show interactive feedback. e.g. mark edits, show number of edits, show diff in separate plane or so
-
-https://docs.python.org/3/library/curses.html
-- too complex but at the same time too limited?
-
-https://github.com/bczsalba/pytermgui (1.2k stars)
-- limited, no real text editor
-
-https://urwid.org/examples/index.html (2.5k stars)
-- edit example: https://github.com/urwid/urwid/blob/master/examples/edit.py
-
-https://github.com/prompt-toolkit/python-prompt-toolkit (7.9k stars)
-- too complex...? similar as curses...
-
-https://github.com/pfalcon/picotui (0.7k stars)
-- good enough? editor: https://github.com/pfalcon/picotui/blob/master/picotui/editor.py
-- another editor: https://github.com/pfalcon/picotui/blob/master/seditor.py
-
-https://github.com/Textualize/textual (13k stars)
-- async framework, I don't want that...
-
-(Or coding some line edit by hand, should not be too difficult...?)
 """
 
+# Original code:
 # https://github.com/pfalcon/picotui/blob/master/seditor.py
 #
 # Very simple VT100 terminal text editor widget
@@ -41,7 +13,7 @@ https://github.com/Textualize/textual (13k stars)
 # https://en.wikipedia.org/wiki/ANSI_escape_code#Terminal_input_sequences
 # https://invisible-island.net/xterm/ctlseqs/ctlseqs.html#h2-The-Alternate-Screen-Buffer
 
-import sys
+from __future__ import annotations
 import tty
 import termios
 import os
